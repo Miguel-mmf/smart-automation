db8c55f94bbce55f5029d3fdf172c15230c24878
rafa-per <116853932+rafa-per@users.noreply.github.com>
2024-04-10 16:40:44 -0300

    comit

--- utils/create_figures.py
+++ utils/create_figures.py
@@ -1,14 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from distutils.dir_util import mkpath
 
-def difference_plot(real_data, predicted_data, filename=' ', save=False, return_fig=False):
-    """This function will create a figure with the real and predicted power and difference area in another plot.
+
+def difference_plot(
+        real_data,
+        predicted_data,
+        xy_labels={
+            'x': 'Pontos',
+            'y': 'Potência (MW)'
+        },
+        legends={
+            'ax0': ['Potência Real','Potência Estimada'],
+            'ax1': ['Diferença entre Potência Real e Estimada']
+        },
+        filename=' ',
+        save=False,
+        formats=['eps', 'png', 'pdf'],
+        return_fig=False
+    ):
+    """This function creates a figure with two subplots: the first one shows the real and predicted data, and the second one shows the difference between the real and predicted data.
 
     Args:
-        real_data (_type_): _description_
-        predicted_data (_type_): _description_
+        real_data (Series): A series with the real data.
+        predicted_data (Series): A series with the predicted data.
+        xy_labels (dict, optional): _description_. Defaults to { 'x': 'Pontos', 'y': 'Potência (MW)' }.
+        legends (dict, optional): _description_. Defaults to { 'ax0': ['Potência Real','Potência Estimada'], 'ax1': ['Diferença entre Potência Real e Estimada'] }.
+        filename (str, optional): _description_. Defaults to ' '.
+        save (bool, optional): _description_. Defaults to False.
+        formats (list, optional): _description_. Defaults to ['eps', 'png', 'pdf'].
+        return_fig (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        matplotlib.figure.Figure: A figure with two subplots.
     """
+
     fig, axs = plt.subplots(2, 1, figsize=(12, 8), dpi=600, sharex=True)
 
     axs[0].plot(real_data, lw=2, color='Red', marker='s', markersize=3)
@@ -23,25 +50,26 @@ def difference_plot(real_data, predicted_data, filename=' ', save=False, return_
         alpha=0.5
     )
 
-    axs[0].grid(True)
-    axs[1].grid(True)
-    axs[0].set_ylabel('Potência (MW)')
-    axs[1].set_ylabel('Potência (MW)')
-    axs[0].set_xlabel('Pontos')
-    axs[1].set_xlabel('Pontos')
-    axs[0].legend(['Potência Real','Potência Estimada'])
-    axs[1].legend(['Diferença entre Potência Real e Estimada'])
-    
+    for i in range(2):
+        axs[i].grid(True)
+        axs[i].set_ylabel(xy_labels['y'])
+        axs[i].set_xlabel(xy_labels['x'])
+        axs[i].legend(legends[f'ax{i}'])
+
     plt.tight_layout()
 
     if save:
         if filename == ' ':
-            f = key.upper()
+            f = './figures/difference_plot'
         else:
-            f = filename + '_' + key.upper()
-        #plt.savefig(f'{filename}.eps', format='eps', dpi=600)
-        plt.savefig(f'{f}.png', format='png', dpi=600)
-        #plt.savefig(f'{filename}.pdf', format='pdf', dpi=600)
+            mkpath('/'.join(filename.split('/')[:-1]))
+            f = filename
+
+        for format in formats:
+            plt.savefig(f'{f}.{format}', format=format, dpi=600)
+
 
     if return_fig:
         return fig, axs
+    else:
+        plt.show()
